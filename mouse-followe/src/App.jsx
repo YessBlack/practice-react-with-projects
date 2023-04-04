d5f88b74afc9299e5049583f840f57cc20dc9091
Angelica Garcia <anyegar36@gmail.com>
2023-04-03 19:31:33 -0500

    add mouse-follower

---
+++ mouse-followe/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { FollowMouse } from './components/FollowMouse'
+
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
