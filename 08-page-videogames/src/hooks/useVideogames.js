e7b42ee8f247569cf0d6e0ddb1883203e4123d93
Angelica Garcia <anyegar36@gmail.com>
2023-04-15 15:58:00 -0500

    use ref

--- 08-page-videogames/src/hooks/useVideogames.js
+++ 08-page-videogames/src/hooks/useVideogames.js
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchGames } from '../services/games'
 
 export function useVideogames ({ search }) {
   const [responseGames, setResponseGames] = useState({})
   const [loading, setLoading] = useState(false)
+  const prevSearch = useRef(search)
 
   const getVideogames = async () => {
-    setLoading(true)
-    const data = await searchGames({ search })
-    setResponseGames(data)
-    setLoading(false)
+    if (search === prevSearch.current) return
+    try {
+      setLoading(true)
+      prevSearch.current = search
+      const data = await searchGames({ search })
+      setResponseGames(data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return { videogames: responseGames, loading, getVideogames }
