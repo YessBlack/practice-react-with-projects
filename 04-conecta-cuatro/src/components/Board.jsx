bc908fb402469d34f5ea2a099600c802724e7196
Angelica Garcia <anyegar36@gmail.com>
2023-04-26 17:28:56 -0500

    add feature turn and color board

--- 04-conecta-cuatro/src/components/Board.jsx
+++ 04-conecta-cuatro/src/components/Board.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Player } from './Player'
 
+const TURNS = {
+  PLAYER_ONE: 1,
+  PLAYER_TWO: 2
+}
+
 export function Board () {
   const [board, setBoard] = useState(Array(42).fill(null))
   const [position, setPosition] = useState({ x: 650 })
   const [isInside, setIsInside] = useState(false)
+  const [turn, setTurn] = useState(TURNS.PLAYER_ONE)
 
   useEffect(() => {
     const handleMove = (event) => {
@@ -29,24 +35,137 @@ export function Board () {
     setIsInside(false)
   }
 
+  const handleClickBoard = (i) => {
+    if (i >= 0 && i <= 5) {
+      for (let j = 5; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+
+    if (i >= 6 && i <= 11) {
+      for (let j = 11; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+
+    if (i >= 12 && i <= 17) {
+      for (let j = 17; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+
+    if (i >= 18 && i <= 23) {
+      for (let j = 23; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+
+    if (i >= 24 && i <= 29) {
+      for (let j = 29; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+
+    if (i >= 30 && i <= 35) {
+      for (let j = 35; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+
+    if (i >= 36 && i <= 41) {
+      for (let j = 41; j >= i; j--) {
+        if (board[j] === null) {
+          setBoard((prev) => {
+            const newBoard = [...prev]
+            newBoard[j] = turn
+            return newBoard
+          })
+          setTurn(turn === TURNS.PLAYER_ONE ? TURNS.PLAYER_TWO : TURNS.PLAYER_ONE)
+          return
+        }
+      }
+    }
+  }
+
+  console.log(board)
+
   return (
     <>
-      <div className={`${isInside ? 'cursor-pointer' : 'cursor-default'} absolute bg-player-one w-[30px] h-[30px] border-t-8 border-2 rounded-b-xl top-[90px] left-[20px] mr-[-310px]`} style={{ transform: `translate(${position.x}px)` }} />
-
       <div className='flex items-center gap-5 flex-wrap'>
-        <Player numberPlayer={1} move={0} />
+        <div className={`${isInside ? 'cursor-pointer' : 'cursor-default'} absolute bg-player-one w-[30px] h-[30px] border-t-8 border-2 rounded-b-xl top-[90px] left-[10px]`} style={{ transform: `translate(${position.x}px)` }} />
+        <Player
+          numberPlayer={1}
+          move={0}
+          classNameBorder={turn === TURNS.PLAYER_ONE ? 'border-player-one' : ''}
+        />
         <section
           className='grid grid-rows-6 grid-flow-col gap-4 bg-white mt-[20px] p-3 rounded-xl border-2 border-b-8'
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
           {
-        board.map((_, i) => (
-          <div key={i} className='bg-background-circle p-3 rounded-full border-2 w-[55px] h-[55px] border-t-8' />
+        board.map((el, i) => (
+          <div
+            key={i}
+            className={`bg-background-circle p-3 rounded-full border-2 w-[55px] h-[55px] border-t-8 ${el === 1 ? 'bg-player-one' : el === 2 ? 'bg-player-two' : ''}`}
+            onClick={() => handleClickBoard(i)}
+          />
         ))
       }
         </section>
-        <Player numberPlayer={2} move={0} />
+        <Player
+          numberPlayer={2}
+          move={0}
+          classNameBorder={turn === TURNS.PLAYER_TWO ? 'border-player-two' : ''}
+        />
       </div>
     </>
 
--- 04-conecta-cuatro/src/components/Player.jsx
+++ 04-conecta-cuatro/src/components/Player.jsx
@@ -1,8 +1,8 @@
-export function Player ({ numberPlayer, move }) {
+export function Player ({ numberPlayer, move, classNameBorder }) {
   const classNameBG = numberPlayer === 1 ? 'text-player-one' : 'text-player-two'
 
   return (
-    <article className='p-1 w-[100px] h-[130px] border-2 border-b-8 rounded-xl bg-white text-center'>
+    <article className={`p-1 w-[100px] h-[130px] border-2 border-b-8 rounded-xl bg-white text-center ${classNameBorder}`}>
       <span className={`icon-happy ${classNameBG} text-3xl`} />
       <p className='text-1xl font-bold'>PLAYER {numberPlayer}</p>
       <p className='text-4xl font-bold'>{move}</p>
