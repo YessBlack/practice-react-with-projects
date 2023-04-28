f040470097781d354270cb3933c309a38a3f6d3d
Angelica Garcia <anyegar36@gmail.com>
2023-04-28 18:12:07 -0500

    add winner and refactor

---
+++ 04-conecta-cuatro/src/components/Winner.jsx
@@ -0,0 +1,30 @@
+import { useBoard } from '../hooks/useBoard'
+import { Player } from './Player'
+
+export function Winner ({ numberPlayer, resetGame }) {
+  return (
+    <>
+      <section
+        className='absolute w-[100vw] h-[100vh] top-0 left-0 grid place-items-center bg-[rgba(0, 0, 0, 0.7)] rounded-xl'
+      >
+        <div className='bg-[#111] h-[300px] w-[320px] rounded-xl flex flex-col justify-center items-center gap-4 p-3'>
+          <h2 className='text-white'>Felicitaciones</h2>
+
+          <header className=''>
+            {
+              numberPlayer === 1 || numberPlayer === 2
+                ? <Player numberPlayer={numberPlayer} move={4} />
+                : <p className='text-white'>Empate</p>
+            }
+          </header>
+
+          <footer>
+            <button className='bg-button-color p-2 rounded-xl' onClick={resetGame}>Empezar de nuevo</button>
+          </footer>
+        </div>
+
+      </section>
+    </>
+
+  )
+}
